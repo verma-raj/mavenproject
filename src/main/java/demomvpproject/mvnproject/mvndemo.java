be7ceb6f831e177d21edb0dba0c34e292a84bb0e
verma-raj <jobhunt.verma@gmail.com>
2022-10-08 00:00:34 +0530

    Initial commit

---
+++ src/main/java/demomvpproject/mvnproject/mvndemo.java
@@ -0,0 +1,10 @@
+package demomvpproject.mvnproject;
+
+public class mvndemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi !! Testing Maven here");
+	}
+
+}
